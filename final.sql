9d2cac1067a925edac685a250b25e5dd63ba82c8
Nate <77016702+NathanKochhar@users.noreply.github.com>
2024-05-09 12:18:29 -0700

    Update final.sql

--- final.sql
+++ final.sql
@@ -1,6 +1,6 @@
 #create db
-CREATE DATABASE IF NOT EXISTS rnaseq_analysis;
-USE rnaseq_analysis;
+CREATE DATABASE nkochha1_final;
+USE nkochha1_final;
 
 #genomes
 CREATE TABLE IF NOT EXISTS genomes (
@@ -22,6 +22,10 @@ CREATE TABLE IF NOT EXISTS run_info (
     FOREIGN KEY (genome) REFERENCES genomes(species)
 );
 
+#populate run_info
+INSERT INTO run_info (r1_fastq_path, r2_fastq_path, genome, sequencing_type) VALUES
+('/home/nathankochhar/test_fastqs/test_r1.fastq', '/home/nathankochhar/test_fastqs/test_r1.fastq', 'human', 'single');
+
 #outputs
 CREATE TABLE IF NOT EXISTS outputs (
     output_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -35,3 +39,10 @@ CREATE TABLE IF NOT EXISTS outputs (
     FOREIGN KEY (run_id) REFERENCES run_info(run_id)
 );
 
+#populate outputs
+INSERT INTO outputs (run_id, fastqc_R1_path, fastqc_R2_path, trimmed_R1_path, trimmed_R2_path, aligned_bam_path, ReadsPerGene_path) VALUES
+(1, '/home/nathankochhar/fastqc/test_r1_fastqc.html', '/home/nathankochhar/fastqc/test_r1_fastqc.html', '/home/nathankochhar/test_fastqs/trimmed_r1.fastq', '/home/nathankochhar/trimmed_fastqs/test_r1.fastq', '/home/nathankochhar/STAR_outs/testAligned.sortedByCoord.out.bam', '/home/nathankochhar/STAR_outs/testReadsPerGene.out.tab');
+
+SELECT * FROM genomes;
+SELECT * FROM run_info;
+SELECT * FROM outputs;
